354310360f50aa6ab9b0712ae08f3517c90e822d
magalaReuben <magalareuben60@gmail.com>
2024-01-27 05:53:08 +0300

    new screens

--- lib/screens/servicesUnderSubCategory/servicesUnderSubcategory.dart
+++ lib/screens/servicesUnderSubCategory/servicesUnderSubcategory.dart
@@ -1,17 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class ServicesUnderSubCategory extends StatefulWidget {
+class ServicesUnderSubCategory extends ConsumerStatefulWidget {
   static String routeName = "/servicesUnderSubCategory";
   const ServicesUnderSubCategory({super.key});
 
   @override
-  State<ServicesUnderSubCategory> createState() =>
+  ConsumerState<ServicesUnderSubCategory> createState() =>
       _ServicesUnderSubCategoryState();
 }
 
-class _ServicesUnderSubCategoryState extends State<ServicesUnderSubCategory> {
+class _ServicesUnderSubCategoryState
+    extends ConsumerState<ServicesUnderSubCategory> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text("Services Under Sub Category"),
+        ),
+        body: const Padding(
+          padding: EdgeInsets.all(8.0),
+          child: SingleChildScrollView(
+            child: Column(
+              children: [],
+            ),
+          ),
+        ));
   }
 }
