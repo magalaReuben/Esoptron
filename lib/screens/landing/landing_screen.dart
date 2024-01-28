d770d539a8b5020a34c4f7128dcfe1ebc00598bb
magalaReuben <magalareuben60@gmail.com>
2024-01-28 08:07:20 +0300

    some simple additions

--- lib/screens/landing/landing_screen.dart
+++ lib/screens/landing/landing_screen.dart
@@ -10,6 +10,7 @@ import 'package:esoptron_salon/screens/wallet/wallet_screen.dart';
 import 'package:esoptron_salon/widgets/bottom_navigation_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:sliding_clipped_nav_bar/sliding_clipped_nav_bar.dart';
 
 class LandingScreen extends StatefulWidget {
   static String routeName = '/landing';
@@ -29,6 +30,7 @@ class _LandingScreenState extends State<LandingScreen> {
     const ServiceProviderMenu()
   ];
   bool? isCustomer = true;
+  late PageController _pageController;
 
   getClientType() async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
@@ -51,41 +53,86 @@ class _LandingScreenState extends State<LandingScreen> {
     getClientType();
   }
 
+  void changePage(int index) {
+    setState(() {
+      currentTab = index;
+    });
+    _pageController.animateToPage(currentTab,
+        duration: const Duration(milliseconds: 400), curve: Curves.easeOutQuad);
+  }
+
   final PageStorageBucket bucket = PageStorageBucket();
   Widget currentScreen = const HomeScreen();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: PageStorage(
-        bucket: bucket,
-        child: currentScreen,
+      body: Column(
+        children: [
+          Expanded(
+            child: PageView(
+              physics: const NeverScrollableScrollPhysics(),
+              controller: _pageController,
+              children: screens,
+            ),
+          ),
+        ],
       ),
-      bottomNavigationBar: BottomNavWidget(
-        currentIndex: currentTab,
-        isCustomer: isCustomer!,
-        onChange: (index) async {
-          setState(() {
-            currentTab = index;
-            switch (index) {
-              case 0:
-                currentScreen = const HomeScreen();
-                break;
-              case 1:
-                currentScreen = const ServiceScreen();
-                break;
-              case 2:
-                currentScreen = const TrackScreen();
-                break;
-              case 3:
-                currentScreen = const ProfileScreen();
-                break;
-              case 4:
-                currentScreen = const ServiceProviderMenu();
-                break;
-            }
-          });
-        },
+      bottomNavigationBar: SlidingClippedNavBar(
+        backgroundColor: const Color.fromARGB(255, 58, 57, 57),
+        onButtonPressed: changePage,
+        iconSize: 30,
+        activeColor: const Color.fromARGB(255, 1, 165, 69),
+        inactiveColor: Colors.white,
+        selectedIndex: currentTab,
+        barItems: <BarItem>[
+          BarItem(
+            icon: Icons.css,
+            title: 'CSS',
+          ),
+          BarItem(
+            icon: Icons.html,
+            title: 'HTML',
+          ),
+          BarItem(
+            icon: Icons.javascript,
+            title: 'JavaScript',
+          ),
+          BarItem(
+            icon: Icons.flutter_dash,
+            title: 'Flutter',
+          ),
+        ],
       ),
+      // body: PageStorage(
+      //   bucket: bucket,
+      //   child: currentScreen,
+      // ),
+      // bottomNavigationBar: BottomNavWidget(
+      //   currentIndex: currentTab,
+      //   isCustomer: isCustomer!,
+      //   onChange: (index) async {
+      //     setState(() {
+      //       currentTab = index;
+      //       switch (index) {
+      //         case 0:
+      //           currentScreen = const HomeScreen();
+      //           break;
+      //         case 1:
+      //           currentScreen = const ServiceScreen();
+      //           break;
+      //         case 2:
+      //           currentScreen = const TrackScreen();
+      //           break;
+      //         case 3:
+      //           currentScreen = const ProfileScreen();
+      //           break;
+      //         case 4:
+      //           currentScreen = const ServiceProviderMenu();
+      //           break;
+      //       }
+      //     });
+      //   },
+      // ),
     );
   }
 }
